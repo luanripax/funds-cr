475c96efca28387a72801837a0357f105f4409c8
Luan Thome <luan_ripax@hotmail.com>
2023-06-22 10:56:00 -0300

    feat/added Sora fonts

---
+++ src/theme/index.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primary: "#770FDF",
+  },
+  fonts: {
+    bold: "Sora-Bold",
+    light: "Sora-Light",
+    medium: "Sora-Medium",
+    regular: "Sora-Regular",
+    thin: "Sora-Thin",
+  },
+};
+
+export { theme };
