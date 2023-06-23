e21152c01f6a8c8ae38b0650930b362dcbea06aa
Luan Thome <luan_ripax@hotmail.com>
2023-06-23 11:51:10 -0300

    fix/fix icons and tab height in iOS

--- src/navigation/tab/index.tsx
+++ src/navigation/tab/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import HomeScreen from "../../screens/Tab/Home/home.screen";
 import PortFolioScreen from "../../screens/Tab/Portfolio/portfolio.screen";
 import TradeScreen from "../../screens/Tab/Trade/trade.screen";
 import { SvgXml } from "react-native-svg";
 import svgIcon from "../../assets/svg";
 import { theme } from "../../theme";
+import { Platform } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
@@ -21,21 +21,28 @@ const TabNavigator = (): JSX.Element => {
           fontFamily: theme.fonts.regular,
           paddingBottom: 10,
         },
-        tabBarStyle: { height: 70, paddingVertical: 10 },
+        tabBarStyle: {
+          height: Platform.OS === "ios" ? 100 : 70,
+          paddingVertical: 10,
+        },
       }}
     >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarIcon: ({ color, size }) => <SvgXml xml={svgIcon.homeTab} />,
+          tabBarIcon: ({ color, size }) => (
+            <SvgXml xml={svgIcon.homeTab} color={color} />
+          ),
         }}
       />
       <Tab.Screen
         name="Trade"
         component={TradeScreen}
         options={{
-          tabBarIcon: ({ color, size }) => <SvgXml xml={svgIcon.tradeTab} />,
+          tabBarIcon: ({ color, size }) => (
+            <SvgXml xml={svgIcon.tradeTab} color={color} />
+          ),
         }}
       />
       <Tab.Screen
@@ -43,7 +50,7 @@ const TabNavigator = (): JSX.Element => {
         component={PortFolioScreen}
         options={{
           tabBarIcon: ({ color, size }) => (
-            <SvgXml xml={svgIcon.portfolioTab} />
+            <SvgXml xml={svgIcon.portfolioTab} color={color} />
           ),
         }}
       />
